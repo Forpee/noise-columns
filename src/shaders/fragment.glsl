a6a8f3113dd43d09c8f3ed60f1ce1d114b76b65b
Forpee <khalilhassam3@gmail.com>
2022-05-19 19:12:12 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -142,6 +142,15 @@ float cnoise(vec4 P){
     return 2.2*n_xyzw;
 }
 
+float aastep(float threshold,float value){
+    #ifdef GL_OES_standard_derivatives
+    float afwidth=length(vec2(dFdx(value),dFdy(value)))*.70710678118654757;
+    return smoothstep(threshold-afwidth,threshold+afwidth,value);
+    #else
+    return step(threshold,value);
+    #endif
+}
+
 void main()
 {
     gl_FragColor=vec4(vUv,1.,1.);
